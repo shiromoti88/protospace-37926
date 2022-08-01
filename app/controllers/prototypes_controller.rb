93681f291069dc48b867236cd788be01a186d712
shiromoti88 <1007.0920j@gmail.com>
2022-08-01 22:04:23 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -49,3 +49,5 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+  
+end
